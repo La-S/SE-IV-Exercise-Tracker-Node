e8140afc16d920fdfb0d7779d81b5d74fda26687
Lance Skinner <sirlancelot2604@outlook.com>
2025-10-27 10:06:41 -0500

    minor improvements

--- app/models/exerciseTemplate.model.ts
+++ app/models/exerciseTemplate.model.ts
@@ -11,10 +11,10 @@ const ExerciseTemplate = SequelizeInstance.define("exerciseTemplate", {
     type: Sequelize.STRING(100),
   },
   type: {
-    type: Sequelize.ENUM('strength', 'cardio', 'mobility', 'other'),
+    type: Sequelize.ENUM("strength", "cardio", "mobility", "other"),
   },
   muscle_group: {
-    type: Sequelize.ENUM("bicep", "tricep", "forearm", "shoulders", "back", "chest", "core", "quad", "hamstring", "calf", "glute"),
+    type: Sequelize.ENUM("bicep", "tricep", "forearm", "shoulders", "back", "chest", "core", "quad", "hamstring", "calf", "glute", "other"),
   },
 });
 
