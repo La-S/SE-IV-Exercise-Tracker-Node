456be73675dcea3e96745455157ea6272535abfc
JohnEveryOC <j.every@eagles.oc.edu>
2025-10-30 15:30:57 -0500

    add workout routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 import AuthRoutes from "./auth.routes.js";
 import UserRoutes from "./user.routes.js";
 import Team from "./team.routes.js";
+import WorkoutRoutes from "./workout.routes.js"
 
 
 const router = Router();
@@ -10,5 +11,6 @@ const router = Router();
 router.use("/", AuthRoutes);
 router.use("/users", UserRoutes);
 router.use("/team", Team);
+router.use("/workout", WorkoutRoutes);
 
 export default router;
