edfa3306480ea6636cb4659bfde54a0ea0589a0b
JohnEveryOC <j.every@eagles.oc.edu>
2025-11-03 18:27:40 -0600

    fix routes

--- app/routes/exercise.routes.js
+++ app/routes/exercise.routes.js
@@ -20,7 +20,7 @@
   // Delete a User with id
   router.delete("/:id", [authenticate], exercise.delete);
 
-  router.get(":id/sets", exercise.getSets);
+  router.get("/:id/sets", exercise.getSets);
 
   export default router;
 
