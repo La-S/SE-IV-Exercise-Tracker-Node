489133beefcb00de77d7b4a3384f43f78894f95a
JohnEveryOC <j.every@eagles.oc.edu>
2025-11-05 09:46:17 -0600

    remove aliases

--- app/models/index.js
+++ app/models/index.js
@@ -33,33 +33,28 @@ db.team.belongsToMany(db.user,
 db.exercise.hasMany(db.set,
     { foreignKey: { name: "exercise_id", allowNull: false }, onDelete: "CASCADE" });
 db.set.belongsTo(db.exercise,
-    { as: "exercise" },
     { foreignKey: { name: "exercise_id", allowNull: false }, onDelete: "CASCADE" });
 
 //an exercise template is used in many exercises
 db.exerciseTemplate.hasMany(db.exercise,
     { foreignKey: { name: "exercise_template_id", allowNull: false }, onDelete: "CASCADE" });
 db.exercise.belongsTo(db.exerciseTemplate,
-    { as: "exerciseTemplate" },
     { foreignKey: { name: "exercise_template_id", allowNull: false }, onDelete: "CASCADE" });
 
 //a workout has many exercises
 db.workout.hasMany(db.exercise,
     { foreignKey: { name: "workout_id", allowNull: false }, onDelete: "CASCADE" });
 db.exercise.belongsTo(db.workout,
-    { as: "workout" },
     { foreignKey: { name: "workout_id", allowNull: false }, onDelete: "CASCADE" });
 
 //two users are tied to each workout, coach and user. 
 db.user.hasMany(db.workout,
     { foreignKey: { name: "user_id", allowNull: false }, onDelete: "CASCADE" });
 db.workout.belongsTo(db.user,
-    { as: "user" },
     { foreignKey: { name: "user_id", allowNull: false }, onDelete: "CASCADE" });
 db.user.hasMany(db.workout,
     { foreignKey: "coach_id" });
 db.workout.belongsTo(db.user,
-    { as: "coach" },
     { foreignKey: "coach_id" });
 
 db.sequelize.sync();
