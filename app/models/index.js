9115cea7fdc616cc5c6c9fe30bba5745d064f72c
JohnEveryOC <122403458+JohnEveryOC@users.noreply.github.com>
2025-11-13 14:21:39 -0600

    Remove force true

--- app/models/index.js
+++ app/models/index.js
@@ -72,5 +72,5 @@ db.user.hasMany(db.userStats,
 db.userStats.belongsTo(db.user,
     { foreignKey: { name: "user_Id", allowNull: false }, onDelete: "CASCADE" });
 
-db.sequelize.sync({force: true});
+db.sequelize.sync();
 export default db;
